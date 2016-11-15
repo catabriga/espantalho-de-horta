dda5a69834bd7d455cd713b0c6d6d012c41dab48
lucas <catabriga90@gmail.com>
2016-11-15 19:31:05 -0200

    Testing DHT sensor

--- dhtReader.py
+++ dhtReader.py
@@ -2,7 +2,7 @@ import Adafruit_DHT
 
 dhtPin = 23
 
-def: getDhtData():
+def getDhtData():
 
     humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, DHTpin)
     return (humidity, temperature)
